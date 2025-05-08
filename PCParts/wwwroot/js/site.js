c5b1c4ffce32a4b7f094669d06513abc75605949
tutasmaster <ruisims@gmail.com>
2025-05-08 12:35:31 +0100

    Adicionado a view Localizacao e dropdown correspondente na barra de navegação. TODO: Locais por determinar.

--- PCParts/wwwroot/js/site.js
+++ PCParts/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function filtrarLocalizacoes(){
+    let elementoFiltro = document.getElementById('filtro');
+    let filtro = elementoFiltro.value.toUpperCase();
+    let filtroDropdown = document.getElementById('filtroDropdown');
+    let elementos = filtroDropdown.getElementsByTagName('a');
+    for(let i = 0; i < elementos.length; i++){
+        let value = elementos[i].textContent.toUpperCase();
+        if (filtro === "" || value.indexOf(filtro) > -1) {
+            elementos[i].style.display = "";
+        } else {
+            elementos[i].style.display = "none";
+        }
+    }
+}
